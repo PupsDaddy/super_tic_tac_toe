0483847eacd9b52cb036cb5f3341bcdeb279d4f4
Yevgeny Dobrinets <ydobrinetsr@mail.ru>
2025-01-29 23:30:39 +0300

    finishing game logic; creating ui

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,1 @@
-WIDTH = 600HEIGHT = 600FPS = 60SCALE = 30WINNING_COMBINATIONS = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+WIDTH = 600HEIGHT = 600FPS = 60SCALE = 30WINNING_COMBINATIONS = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))SCREEN_COLOR = 'white'ELEMENT_COLOR = (200, 200, 200)ELEMENT_SIZE = 28CROSS_COLOR = 'red'CIRCLE_COLOR = 'blue'
